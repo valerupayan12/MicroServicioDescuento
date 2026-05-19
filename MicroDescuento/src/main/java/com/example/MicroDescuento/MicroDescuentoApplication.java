ff5140dd0310f18e2f4971b3914a43075c71b389
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 13:27:02 -0400

    Add files via upload

---
+++ MicroDescuento/src/main/java/com/example/MicroDescuento/MicroDescuentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroDescuento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroDescuentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroDescuentoApplication.class, args);
+	}
+
+}
---
+++ MicroDescuento/src/test/java/com/example/MicroDescuento/MicroDescuentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroDescuento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroDescuentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
